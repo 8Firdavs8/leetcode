4437bad5e0b3f8df91f63667396641086fbf965d
8Firdavs8 <atabfi01@luther.edu>
2022-01-16 21:51:25 -0600

    pick even numbers

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -4,3 +4,15 @@ for i in range(9):
     y = i*i
     x.append(y)
 print(x)
+
+# Pick Even numbers and return them in a list:
+myfunc(5,6,7,8)
+
+emptyList = []
+def myfunc (*args):
+    for x in args:
+        if x % 2 == 0:
+            emptyList.append(x)
+        else:
+            pass
+    return emptyList
